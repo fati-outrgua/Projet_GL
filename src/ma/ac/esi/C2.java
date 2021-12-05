a2e036428cfbabee217485562b4cece981e526a6
Outrgua <fatimazahraouterga@gmail.com>
2021-12-05 23:24:20 +0100

    la création de la dexième classe C2

---
+++ src/ma/ac/esi/C2.java
@@ -0,0 +1,11 @@
+package ma.ac.esi;
+
+public class C2 {
+
+	public static void main(String[] args) {
+		System.out.println("c'est la deuxième classe");
+		// TODO Auto-generated method stub
+
+	}
+
+}
