0de2ccd7e655d3bb942969470ba90d7775e27162
Outrgua <fatimazahraouterga@gmail.com>
2021-12-06 09:30:02 +0100

    commit classe C2

--- src/ma/ac/esi/C2.java
+++ src/ma/ac/esi/C2.java
@@ -3,7 +3,7 @@ package ma.ac.esi;
 public class C2 {
 
 	public static void main(String[] args) {
-		System.out.println("c'est la deuxième classe");
+		System.out.println("c'est la deuxième classe C2");
 		// TODO Auto-generated method stub
 
 	}
